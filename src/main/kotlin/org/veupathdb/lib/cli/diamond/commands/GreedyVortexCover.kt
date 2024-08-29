b4f97821df866ed5d2dfe3d539a2429f3362c3b5
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-08-29 09:34:57 -0400

    raw greedy vortex cover options

--- src/main/kotlin/org/veupathdb/lib/cli/diamond/commands/GreedyVortexCover.kt
+++ src/main/kotlin/org/veupathdb/lib/cli/diamond/commands/GreedyVortexCover.kt
@@ -8,3 +8,11 @@ interface GreedyVortexCover
   , GeneralOutputHeaderOptionContainer
   , GeneralOutputOptionContainer
   , ClusteringReassignOptionContainer
+{
+  ("centroid-out", 0, "Output file for centroids", centroid_out)
+  ("edges", 0, "Input file for greedy vertex cover", edges)
+  ("edge-format", 0, "Edge format for greedy vertex cover (default/triplet)", edge_format)
+  ("symmetric", 0, "Edges are symmetric", symmetric)
+  ("no-reassign", 0, "Do not reassign to closest representative", no_gvc_reassign)
+  ("connected-component-depth", 0, "Depth to cluster connected components", connected_component_depth);
+}
