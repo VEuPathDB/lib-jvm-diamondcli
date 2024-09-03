8e1b0c69495422f5cb8232de0ace9fdf71bbabf0
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-09-03 17:54:12 -0400

    sensitivity options

--- src/main/kotlin/org/veupathdb/lib/cli/diamond/opts/AlignerSensitivityOptionContainer.kt
+++ src/main/kotlin/org/veupathdb/lib/cli/diamond/opts/AlignerSensitivityOptionContainer.kt
@@ -17,14 +17,92 @@
 
 package org.veupathdb.lib.cli.diamond.opts
 
+import com.fasterxml.jackson.annotation.JsonGetter
+import com.fasterxml.jackson.annotation.JsonSetter
+
+// TODO: Most of these options should be condensed to an enum, they are mutually
+//       exclusive and configure the same setting to different values.  The only
+//       field in here that _isn't_ part of the enum is [shapes]
 interface AlignerSensitivityOptionContainer {
-  ("faster", 0, "enable faster mode", mode_faster)
-  ("fast", 0, "enable fast mode", mode_fast)
-  ("mid-sensitive", 0, "enable mid-sensitive mode", mode_mid_sensitive)
-  ("shapes-30x10", 0, "enable mode using 30 seed shapes of weight 10", mode_shapes30x10)
-  ("sensitive", 0, "enable sensitive mode)", mode_sensitive)
-  ("more-sensitive", 0, "enable more sensitive mode", mode_more_sensitive)
-  ("very-sensitive", 0, "enable very sensitive mode", mode_very_sensitive)
-  ("ultra-sensitive", 0, "enable ultra sensitive mode", mode_ultra_sensitive)
-  ("shapes", 's', "number of seed shapes (default=all available)", shapes);
+  // enable faster mode
+  // `--faster`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_faster`
+  @get:JsonGetter("faster")
+  @set:JsonSetter("faster")
+  var faster: Boolean
+
+  // enable fast mode
+  // `--fast`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_fast`
+  @get:JsonGetter("fast")
+  @set:JsonSetter("fast")
+  var fast: Boolean
+
+  // enable mid-sensitive mode
+  // `--mid-sensitive`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_mid_sensitive`
+  @get:JsonGetter("midSensitive")
+  @set:JsonSetter("midSensitive")
+  var midSensitive: Boolean
+
+  // enable mode using 30 seed shapes of weight 10
+  // `--shapes-30x10`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_shapes30x10`
+  @get:JsonGetter("shapes30x10")
+  @set:JsonSetter("shapes30x10")
+  var shapes30x10: Boolean
+
+  // enable sensitive mode)
+  // `--sensitive`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_sensitive`
+  @get:JsonGetter("sensitive")
+  @set:JsonSetter("sensitive")
+  var sensitive: Boolean
+
+  // enable more sensitive mode
+  // `--more-sensitive`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_more_sensitive`
+  @get:JsonGetter("moreSensitive")
+  @set:JsonSetter("moreSensitive")
+  var moreSensitive: Boolean
+
+  // enable very sensitive mode
+  // `--very-sensitive`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_very_sensitive`
+  @get:JsonGetter("verySensitive")
+  @set:JsonSetter("verySensitive")
+  var verySensitive: Boolean
+
+  // enable ultra sensitive mode
+  // `--ultra-sensitive`
+  // Default = ``
+  // Type = `bool`
+  // Config Var = `mode_ultra_sensitive`
+  @get:JsonGetter("ultraSensitive")
+  @set:JsonSetter("ultraSensitive")
+  var ultraSensitive: Boolean
+
+  // number of seed shapes (default=all available)
+  // `--shapes` | `-s`
+  // Default = ``
+  // Type = `unsigned`
+  // Config Var = `shapes`
+  @get:JsonGetter("shapes")
+  @set:JsonSetter("shapes")
+  var shapes: UInt
+
 }
