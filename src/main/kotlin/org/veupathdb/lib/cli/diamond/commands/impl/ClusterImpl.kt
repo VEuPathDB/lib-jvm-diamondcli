1b80e74e6fe0e5a02c5a02f83d9604c8224b1fe7
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-09-09 13:52:43 -0400

    clustering

---
+++ src/main/kotlin/org/veupathdb/lib/cli/diamond/commands/impl/ClusterImpl.kt
@@ -0,0 +1,31 @@
+package org.veupathdb.lib.cli.diamond.commands.impl
+
+import org.veupathdb.lib.cli.diamond.commands.Cluster
+import org.veupathdb.lib.cli.diamond.opts.*
+import org.veupathdb.lib.cli.diamond.opts.impl.*
+
+internal class ClusterImpl(
+  private val generalOptions: GeneralOptionContainer = GeneralOptionContainerImpl(),
+  private val generalDBOptions: GeneralDBOptionContainer = GeneralDBOptionContainerImpl(),
+  private val generalOutputHeaderOptions: GeneralOutputHeaderOptionContainer = GeneralOutputHeaderOptionContainerImpl(),
+  private val generalOutputOptions: GeneralOutputOptionContainer = GeneralOutputOptionContainerImpl(),
+  private val alignClusterRealignOptions: AlignerClusteringRealignOptionContainer = AlignerClusteringRealignOptionContainerImpl(),
+  private val alignClusterOptions: AlignerClusteringOptionContainer = AlignerClusteringOptionContainerImpl(),
+  private val clusteringGeneralOptions: ClusteringGeneralOptionContainer = ClusteringGeneralOptionContainerImpl(),
+  private val clusteringReassignOptions: ClusteringReassignOptionContainer = ClusteringReassignOptionContainerImpl(),
+  private val advancedGeneralOptions: AdvancedGeneralOptionContainer = AdvancedGeneralOptionContainerImpl(),
+  private val advancedClusterAlignOptions: AdvancedClusteringAlignmentOptionContainer = AdvancedClusteringAlignmentOptionContainerImpl(),
+  private val hiddenOptions: HiddenOptionContainer = HiddenOptionContainerImpl(),
+)
+  : Cluster
+  , GeneralOptionContainer by generalOptions
+  , GeneralDBOptionContainer by generalDBOptions
+  , GeneralOutputHeaderOptionContainer by generalOutputHeaderOptions
+  , GeneralOutputOptionContainer by generalOutputOptions
+  , AlignerClusteringRealignOptionContainer by alignClusterRealignOptions
+  , AlignerClusteringOptionContainer by alignClusterOptions
+  , ClusteringGeneralOptionContainer by clusteringGeneralOptions
+  , ClusteringReassignOptionContainer by clusteringReassignOptions
+  , AdvancedGeneralOptionContainer by advancedGeneralOptions
+  , AdvancedClusteringAlignmentOptionContainer by advancedClusterAlignOptions
+  , HiddenOptionContainer by hiddenOptions
