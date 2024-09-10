70c95ce4b050f35f765fd505ce69123d338732a1
Elizabeth Paige Harper <foxcapades.io@gmail.com>
2024-09-10 16:33:32 -0400

    doc

--- src/main/kotlin/org/veupathdb/lib/cli/diamond/DiamondExtras.kt
+++ src/main/kotlin/org/veupathdb/lib/cli/diamond/DiamondExtras.kt
@@ -1,5 +1,11 @@
 package org.veupathdb.lib.cli.diamond
 
+/**
+ * Opt-in annotation for features that require the DIAMOND CLI tool to be
+ * compiled with extras enabled.
+ *
+ * @since 1.0.0
+ */
 @RequiresOptIn(
   "This feature requires a DIAMOND tool installation that has been compiled " +
     "with extras enabled."
